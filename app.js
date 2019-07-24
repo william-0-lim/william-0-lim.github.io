fe8853d3a47ea011587a0f1a4359efd5c11b3ed9
williamlim <taegyunlim3@gmail.com>
2019-07-24 15:37:06 -0700

    starting with rich

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+app.use(express.static(__dirname));
+app.use(express.static(__dirname + 'css'));
+
+const port = 8080;
+
+app.get('/', function (req, res){
+	res.sendFile(__dirname + 'index.html');
+});
+
+// app.get('/projects', function(req, res){
+// 	res.sendFile(__dirname + '/projects.html');
+// });
+
+app.listen(port, function(){
+	console.log("Listening on port " + port);
+});
