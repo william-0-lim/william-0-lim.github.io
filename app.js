5405d5be7fa32fade39348502cae64a18ce3952c
William Lim <42948215+william-0-lim@users.noreply.github.com>
2021-05-18 14:24:26 -0700

    Major Changes 2
    Added video for front-page loading

--- app.js
+++ app.js
@@ -9,30 +9,6 @@ app.get('/', function (req, res){
 	res.sendFile(__dirname + 'index.html');
 });
 
-app.get('/projects', function(req, res){
-	res.sendFile(__dirname + '/projects.html');
-});
-
-app.get('/me', function(req, res){
-	res.sendFile(__dirname + '/me.html');
-});
-
-app.get('/projects2', function(req, res){
-	res.sendFile(__dirname + '/projects2.html');
-});
-
-app.get('/hobby', function(req, res){
-	res.sendFile(__dirname + '/hobby.html');
-});
-
-app.get('/experience', function(req, res){
-	res.sendFile(__dirname + '/experience.html');
-});
-
-app.get('/a1', function(req, res){
-	res.sendFile(__dirname + '/a1.html');
-});
-
 app.listen(port, function(){
 	console.log("Listening on port " + port);
 });
